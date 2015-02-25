7437a4592ac1f3f4749277dac5c41f7337f28936
prasethu <praveensng79@gmail.com>
2015-02-24 23:47:42 -0800

    Remote Desktop Emulator Tool

---
+++ RemoteDesktopEmulator/Converters/ResolutionToXOrYConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace RemoteDesktopEmulator
+{
+	public class ResolutionToXOrYConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value == null)
+			{
+				return string.Empty;
+			}
+
+			int index = parameter.ToString().Equals("X", StringComparison.InvariantCultureIgnoreCase) ? 0 : 1;
+			return value.ToString().Split('x')[index].Trim();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ RemoteDesktopEmulator/Converters/TupleToResolutionStringConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace RemoteDesktopEmulator
+{
+	public class TupleToResolutionStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var vals = value as ObservableCollection<Resolution>;
+
+			ObservableCollection<string> resolutionString = new ObservableCollection<string>();
+			
+			foreach (Resolution resolution in vals)
+			{
+				// resolutionString.Add(resolution.Value.Item1 + " x " + resolution.Value.Item2);
+			}
+
+			return resolutionString;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
